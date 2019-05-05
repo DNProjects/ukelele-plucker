c805d7882ec83cdb9e6ba506b79d7d8a6b2f9f4f
Kevin Lee <kevinlee@Kevins-MBP-614.home>
2019-05-05 12:48:58 -0400

    Final Code

--- final/final.ino
+++ final/final.ino
@@ -1,26 +1,26 @@
 #include <Servo.h>
 
 // Pins
-const int servoPin1 = 3;
-const int servoPin2 = 4;
-const int servoPin3 = 5;
-const int servoPin4 = 6;
+const int servoPin1 = 2;
+const int servoPin2 = 3;
+const int servoPin3 = 4;
+const int servoPin4 = 5;
 
 //Motor A
-const int motorPin1  = 7;  // Pin 7 of L293
-const int motorPin2  = 8;  // Pin 8 of L293
+const int motorPin1  = 6;  // Pin 6 of L293
+const int motorPin2  = 7;  // Pin 7 of L293
 
 //Motor B
-const int motorPin3  = 9;  // Pin 9 of L293
-const int motorPin4  = 10; // Pin 10 of L293
+const int motorPin3  = 8;  // Pin 8 of L293
+const int motorPin4  = 9; // Pin 9 of L293
 
 //Motor C
-const int motorPin5  = 11; // Pin 11 of L293
-const int motorPin6  = 12; // Pin 12 of L293
+const int motorPin5  = 10; // Pin 10 of L293
+const int motorPin6  = 11; // Pin 11 of L293
 
 //Motor D
-const int motorPin7  = 13; // Pin 13 of L293
-const int motorPin8  = 14; // Pin 14 of L293
+const int motorPin7  = 12; // Pin 12 of L293
+const int motorPin8  = 13; // Pin 13 of L293
 
 // create servo object to control a servo
 Servo servo1; 
@@ -29,10 +29,10 @@ Servo servo3;
 Servo servo4;
 
 // Populate
-int rotateA[] = {0, 3};
-int rotateB[] = {0, };
-int rotateC[] = {0, };
-int rotateD[] = {0, };
+int rotateA[] = {0, 3000, 3725, 4000};
+int rotateB[] = {0, 3000, 3725, 4000 };
+int rotateC[] = {0, 3000, 3725, 4000 };
+int rotateD[] = {0, 3000, 3725, 4000 };
 
 // Current Rotation Index of DC Motors
 int currA = 0;
@@ -40,9 +40,11 @@ int currB = 0;
 int currC = 0;
 int currD = 0;
 
+int wait = 100;
+
 // Servo objects can be created on most boards
 #define WAIT 1000
-#define ROTATE 35
+#define ROTATE 20
 #define INITIAL 90 - (ROTATE/2)
 
 bool isLeft(int pos) {
@@ -59,7 +61,6 @@ void pick(Servo servo) {
   } else {
     servo.write(INITIAL - ROTATE);
   }
-  return;
 }
 
 int moveDCMotor(int curr, int dest, int motorPinA, int motorPinB, int rotateArr[]) {
@@ -108,13 +109,105 @@ void mySetUp(){
   
   pinMode(motorPin7, OUTPUT);
   pinMode(motorPin8, OUTPUT);
+
+  servo1.write(INITIAL+ROTATE);
+  servo2.write(INITIAL+ROTATE);
+  servo3.write(INITIAL+ROTATE);
+  servo4.write(INITIAL+ROTATE);
+}
+
+
+void pickAllString(){
+  pick(servo1);
+  delay(wait);
+  pick(servo2);
+  delay(wait);
+  pick(servo3);
+  delay(wait);
+  pick(servo4);
+
+  delay(750);
+
+  pick(servo4);
+  delay(wait);
+  pick(servo3);
+  delay(wait);
+  pick(servo2);
+  delay(wait);
+  pick(servo1);
+
+  delay(750);
+
+  pick(servo1);
+  delay(wait);
+  pick(servo2);
+  delay(wait);
+  pick(servo3);
+  delay(wait);
+  pick(servo4);
+
+  delay(750);
+
+  pick(servo4);
+  delay(wait);
+  pick(servo3);
+  delay(wait);
+  pick(servo2);
+  delay(wait);
+  pick(servo1);
+}
+
+void part2And4(){
+   digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  delay(3000);
+  pick(servo4);
+
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  delay(4250);
+  pick(servo4);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  delay(500);
+  
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  delay(3725);
+  pick(servo4);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  delay(500);
+
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  delay(1525);
+  pick(servo4);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  delay(500);
+
+    digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  delay(2000);
+  pick(servo4);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  delay(500);
+
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  delay(1675);  
+
+    digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
 }
 
 void setup() {
   mySetUp();
   
   // Music Here
-  // Maybe rotate motorD back????
+  // Rotate MotorD Back Manually
   /* example of picking a note
     currA = moveDCMotor(currA, 1, motorPin1, motorPin2, rotateA);
     pick(servo1);
@@ -126,43 +219,45 @@ void setup() {
     pick(servo4);
   */
 
-  currA = moveDCMotor(currA, 1, motorPin1, motorPin2, rotateA);
-  pick(servo1);
+  delay(5000);
 
+  
+  // Part 1
+  pick(servo4);
+  delay(2500);
+  pick(servo1);
+  delay(2500);
+  pick(servo4);
+  delay(3000);
+
+  // Part 2
+  part2And4();
+
+  // Part 3
+  pick(servo2);
+  delay(3000);
+  pick(servo3);
+  delay(3000);
+  pick(servo4);
+
+// Part 4
+  part2And4();
+  pick(servo4);
+
+  
+
+  
   // Stop all DC motors
-  analogWrite(motorPin1, 0);
-  analogWrite(motorPin2, 0);
-  
-  analogWrite(motorPin3, 0);
-  analogWrite(motorPin4, 0);
-        
-  analogWrite(motorPin5, 0);
-  analogWrite(motorPin6, 0);
-  
-  analogWrite(motorPin7, 0);
-  analogWrite(motorPin8, 0);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  digitalWrite(motorPin5, LOW);
+  digitalWrite(motorPin6, LOW);
+  digitalWrite(motorPin7, LOW);
+  digitalWrite(motorPin8, LOW);
 }
 
 void loop() {
 
 }
-
-//void loop() {
-//    servo1.write(INITIAL+ROTATE);
-//    delay(WAIT);
-//    servo2.write(INITIAL+ROTATE);
-//    delay(WAIT);
-//    servo3.write(INITIAL+ROTATE);
-//    delay(WAIT);
-//    servo4.write(INITIAL+ROTATE);
-//    delay(WAIT);
-//    
-//    servo4.write(INITIAL-ROTATE);
-//    delay(WAIT);
-//    servo3.write(INITIAL-ROTATE);
-//    delay(WAIT);
-//    servo2.write(INITIAL-ROTATE);
-//    delay(WAIT);
-//    servo1.write(INITIAL-ROTATE);
-//    delay(WAIT);
-//  }
